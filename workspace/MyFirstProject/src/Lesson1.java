1f31c97836adbb41c41eb74db2fd69f2dba8b743
Xingsheng Li <lixs770510@gmail.com>
2015-07-20 22:34:43 -0500

    test sdd

---
+++ workspace/MyFirstProject/src/Lesson1.java
@@ -0,0 +1,19 @@
+
+public class Lesson1 {
+
+	public static void main(String[] args) {
+		int a = 0;
+
+		while (a <= 10)
+		{
+		System.out.println(a);
+		a++;
+		}
+		int b = 0;
+		do {
+			System.out.println(b);
+			b++;
+		} while (b <= 10);
+	}
+
+}
